4ad03e762986cc7230f70b68ca67ec56469fc41d
DESKTOP-SB43F9M\was <wascasla@gmail.com>
2020-07-30 18:37:07 -0300

    add schema usuario query  mutation nuevousuario

--- db/resolvers.js
+++ db/resolvers.js
@@ -1,8 +1,37 @@
+const Usuario = require('../models/Usuario');
+const bcryptjs = require('bcryptjs');
+
 
 // Resolvers
 const resolvers = {
     Query: {
         obtenerCurso: () => "Algo"
+    },
+    Mutation: {
+        nuevoUsuario: async (_, { input }) => {
+
+            const { email, password } = input;
+
+            // revisar si el usuario ya esta registrado
+            const existeUsuario = await Usuario.findOne({ email });
+            if (existeUsuario) {
+                throw new Error('El usuario ya esta registrado');
+            }
+
+            //Hashear su password
+            const salt = await bcryptjs.genSalt(10);
+            input.password = await bcryptjs.hash(password, salt);
+
+
+            // Guardar en la base de datos
+            try {
+                const usuario = new Usuario(input);
+                usuario.save(); // guardar
+                return usuario;
+            } catch (error) {
+                console.log(error);
+            }
+        }
     }
 }
 
--- db/schema.js
+++ db/schema.js
@@ -3,8 +3,27 @@ const { gql } = require('apollo-server');
 //Schema
 const typeDefs = gql`
 
+    type Usuario {
+        id: ID
+        nombre: String
+        apellido: String
+        email: String
+        creado: String
+    }
+
+    input UsuarioInput {
+        nombre: String!
+        apellido: String!
+        email: String!
+        password: String!
+    }
+
     type Query {
         obtenerCurso: String
+    }
+
+    type Mutation {
+        nuevoUsuario(input: UsuarioInput): Usuario
     }
 `;
 
