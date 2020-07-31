5ef607929802c2fccb06b31ac700c69bb2edafd6
wascasla <wascasla@hotmail.com>
2020-07-31 12:14:43 -0300

    ADD autenticacion usuario

--- db/resolvers.js
+++ db/resolvers.js
@@ -1,11 +1,24 @@
 const Usuario = require('../models/Usuario');
 const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+require('dotenv').config({ path: 'variables.env' });
+
+const crearToken = (usuario, secreta, expiresIn) => {
+
+    const { id, email, nombre, apellido } = usuario;
+    
+    return jwt.sign( {id, email, nombre, apellido}, secreta, { expiresIn})
+}
 
 
 // Resolvers
 const resolvers = {
     Query: {
-        obtenerCurso: () => "Algo"
+        obtenerUsuario: async(_, { token }) => {
+            const usuarioId = await jwt.verify(token, process.env.SECRETA);
+
+            return usuarioId
+        }
     },
     Mutation: {
         nuevoUsuario: async (_, { input }) => {
@@ -31,8 +44,34 @@ const resolvers = {
             } catch (error) {
                 console.log(error);
             }
+        },
+
+        autenticarUsuario: async (_, {input}) => {
+
+            const { email, password} = input;
+
+            // Si el usuario existe
+            const existeUsuario = await Usuario.findOne({email});
+            if (!existeUsuario) {
+                throw new Error('El usuario no existe');
+            }
+
+            //  Revisar si el password es correcto
+            const passwordCorrecto = await bcryptjs.compare(password, existeUsuario.password);
+            if(!passwordCorrecto) {
+                throw new Error('El password es Incorrecto');
+            }
+
+            //  Crear el token
+            return {
+                token: crearToken(existeUsuario, process.env.SECRETA, '24h')
+            }
+
         }
+
     }
+
+
 }
 
 module.exports = resolvers;
--- db/schema.js
+++ db/schema.js
@@ -11,6 +11,10 @@ const typeDefs = gql`
         creado: String
     }
 
+    type Token {
+        token: String
+    }
+
     input UsuarioInput {
         nombre: String!
         apellido: String!
@@ -18,12 +22,18 @@ const typeDefs = gql`
         password: String!
     }
 
+    input AutenticarInput {
+        email: String!
+        password: String!
+    }
+
     type Query {
-        obtenerCurso: String
+        obtenerUsuario(token: String!) : Usuario
     }
 
     type Mutation {
         nuevoUsuario(input: UsuarioInput): Usuario
+        autenticarUsuario(input: AutenticarInput) : Token
     }
 `;
 
