ebe38db335a96b05c34877cd46ec693705b33ebd
wascasla <wascasla@hotmail.com>
2020-08-13 11:58:10 -0300

    ADD bearer al jwt del usuario

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const server = new ApolloServer({
         const token = req.headers['authorization'] || '';
         if(token) {
             try {
-                const usuario = jwt.verify(token, process.env.SECRETA);
+                const usuario = jwt.verify(token.replace('Bearer ', ''), process.env.SECRETA);
 
                 //console.log(usuario);
 
